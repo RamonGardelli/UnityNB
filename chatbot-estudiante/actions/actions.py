f3f9bf1623a84461951d598dfac1aa42ed185c26
Maximiliano A. Romero Piaggio <mromeropiaggio@alumnos.exa.unicen.edu.ar>
2021-10-02 15:58:40 -0300

    seguimos respirando xd v3

--- chatbot-estudiante/actions/actions.py
+++ chatbot-estudiante/actions/actions.py
@@ -30,7 +30,8 @@
 ## init actions
 
 from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker, CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SessionStarted, ActionExecuted, SlotSet, EventType
 from rasa_sdk.types import TrackerState
 
